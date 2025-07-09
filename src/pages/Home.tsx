1bf23675465e18a743dde1dffe135b8b9d7bdc72
brenda matias <38896055+brendamatias@users.noreply.github.com>
2025-07-09 18:16:26 -0300

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components";
+import ReactIcon from "@/assets/react.svg?react";
+
+export const Home = () => {
+  return (
+    <div className="space-y-4">
+      <h1>Hello world</h1>
+      <ReactIcon />
+      <Button>Button</Button>
+    </div>
+  );
+};
